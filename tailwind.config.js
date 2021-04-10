f69dbdfc194ad411b92d5c54ae020e2b9a57d2af
Reinis Danne <rei4dan@gmail.com>
2021-04-10 14:55:34 +0300

    css: Apply button styles, style dashboard
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {
+            fontWeight: ['odd'],
+            textAlign: ['odd'],
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
 }
